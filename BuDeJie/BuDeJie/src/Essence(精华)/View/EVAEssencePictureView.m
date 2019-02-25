cac3179123b49219e91f40d1e9fb6d1cd5171686
i8023eva <i8023eva@github>
2019-02-25 10:11:55 +0800

    图片比例更改
    图片比例更改
    注释 gif 代码

--- BuDeJie/BuDeJie/src/Essence(精华)/View/EVAEssencePictureView.m
+++ BuDeJie/BuDeJie/src/Essence(精华)/View/EVAEssencePictureView.m
@@ -28,30 +28,44 @@
     _model = model;
     
 #warning
+    
+    /**
     //    GIF -- imageView 不能播放 gif
     if ([model.image1.lowercaseString hasSuffix:@"gif"]) {
-        
-                        FLAnimatedImageView *imageView = [[FLAnimatedImageView alloc] initWithFrame:CGRectMake(0, 0, model.typeFrame.size.width, model.typeFrame.size.height)];
-                        [imageView sd_setImageWithURL:[NSURL URLWithString:model.image1]];
-                        [self addSubview:imageView];
-                        [self insertSubview:imageView belowSubview:self.gifView];
-                        self.flaImageView = imageView;
-        
-                        self.imageView.hidden = YES;
-        self.gifView.hidden = NO;
-                self.flaImageView.hidden = NO;
+
+        FLAnimatedImageView *imageView = [[FLAnimatedImageView alloc] initWithFrame:CGRectMake(0, 0, model.typeFrame.size.width, model.typeFrame.size.height)];
+        [imageView sd_setImageWithURL:[NSURL URLWithString:model.image1]];
+        [self addSubview:imageView];
+        [self insertSubview:imageView belowSubview:self.gifView];
+        self.flaImageView = imageView;
+
+//        self.imageView.hidden = YES;
+//        self.gifView.hidden = NO;
+//        self.flaImageView.hidden = NO;
     } else {
-                self.imageView.hidden = NO;
-        self.gifView.hidden = YES;
-                self.flaImageView.hidden = YES;
+//        self.imageView.hidden = NO;
+//        self.gifView.hidden = YES;
+//        self.flaImageView.hidden = YES;
+     **/
         
         self.placeholderView.hidden = NO;
         [self.imageView eva_setOriginImage:model.image1 thumbnailImage:model.image0 placeholder:nil completed:^(UIImage * _Nullable image, NSError * _Nullable error, SDImageCacheType cacheType, NSURL * _Nullable imageURL) {
             if (!image) return;
             //控制占位图
             self.placeholderView.hidden = YES;
+            
+            //        图片比例
+            if (model.isBigPicture) {
+                CGFloat imageW = model.typeFrame.size.width;
+                CGFloat imageH = imageW * model.height / model.width;
+                
+                UIGraphicsBeginImageContext(CGSizeMake(imageW, imageH));
+                [self.imageView.image drawInRect:CGRectMake(0, 0, imageW, imageH)];
+                self.imageView.image = UIGraphicsGetImageFromCurrentImageContext();
+                UIGraphicsEndImageContext();
+            }
         }];
-    }
+    //}
     
 
     
@@ -61,16 +75,16 @@
         self.seeBigPictureButton.hidden = NO;
         self.imageView.contentMode = UIViewContentModeTop;//CGFloat scale = 1;  SD_
         self.imageView.clipsToBounds = YES;
-//        图片比例
-        if (self.imageView.image) {
-            CGFloat imageW = model.typeFrame.size.width;
-            CGFloat imageH = imageW * model.height / model.width;
-            
-            UIGraphicsBeginImageContext(CGSizeMake(imageW, imageH));
-            [self.imageView.image drawInRect:CGRectMake(0, 0, imageW, imageH)];
-            self.imageView.image = UIGraphicsGetImageFromCurrentImageContext();
-            UIGraphicsEndImageContext();
-        }
+//        图片比例  --这里图片可能没下载完毕
+//        if (self.imageView.image) {
+//            CGFloat imageW = model.typeFrame.size.width;
+//            CGFloat imageH = imageW * model.height / model.width;
+//
+//            UIGraphicsBeginImageContext(CGSizeMake(imageW, imageH));
+//            [self.imageView.image drawInRect:CGRectMake(0, 0, imageW, imageH)];
+//            self.imageView.image = UIGraphicsGetImageFromCurrentImageContext();
+//            UIGraphicsEndImageContext();
+//        }
     } else {
         self.seeBigPictureButton.hidden = YES;
         self.imageView.contentMode = UIViewContentModeScaleToFill;
